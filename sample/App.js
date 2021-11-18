9e21149b734bcd5bff1b75725c15ba40bee23eea
Huy Dang <caohuy@live.com>
2021-11-18 22:01:41 +0700

    Update sample

--- sample/App.js
+++ sample/App.js
@@ -1,89 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import 'react-native-gesture-handler';
+import {
+  MFMapView,
+  MFDirectionsRenderer
+} from 'react-native-map4d-map';
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  Image,
-  View,
-  Text,
-  Button,
-  NativeModules,
+  StyleSheet
 } from 'react-native';
 
-
-import MapScreen from './MapScreen'
-
-
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-const { Navigation } = require('react-native-navigation');
-
-
-function HomeScreen() {
+function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.safeView}>
+        <MFMapView style={styles.container}
+        camera={{
+          center: { latitude: 16.077491, longitude: 108.221735 },
+          zoom: 16,
+          bearing: 0,
+          tilt: 0,
+        }}
+        >
+          <MFDirectionsRenderer
+            ref={ref => this.directions = ref}
+            routes={[
+              [
+                { latitude: 16.078814, longitude: 108.221592 },
+                { latitude: 16.078972, longitude: 108.223034 },
+                { latitude: 16.075353, longitude: 108.223513 },
+              ],
+              [
+                { latitude: 16.078814, longitude: 108.221592 },
+                { latitude: 16.077491, longitude: 108.221735 },
+                { latitude: 16.077659, longitude: 108.223212 },
+                { latitude: 16.075353, longitude: 108.223513 }
+              ]
+            ]}
+            activedIndex={1}
+            activeStrokeWidth={10}
+            activeStrokeColor="#edf75c"
+            activeOutlineWidth={2}
+            activeOutlineColor="#e3b314"
+            inactiveStrokeWidth={10}
+            inactiveStrokeColor="#3d1294"
+            inactiveOutlineWidth={1}
+            inactiveOutlineColor="#1f113b"
+            originPOIOptions={{
+              coordinate: { latitude: 16.079774, longitude: 108.220534 },
+              title: "Begin Position",
+              titleColor: "#ff0000",
+              icon:{ uri: require('./assets/marker-pink.png') },
+              visible: true
+            }}
+            destinationPOIOptions={{
+              title: "End",
+              titleColor: "#0c4503",
+              icon:{ uri: require('./assets/marker-green.png') },
+              visible: true
+            }}
+            onPress={
+              (event) => {
+                console.log("Press Directions Renderer:", event.nativeEvent)
+                this.directions.setActivedIndex(event.nativeEvent.routeIndex)
+              }
+            }
+          />
+        </MFMapView>
+      </SafeAreaView>
+    </>
   );
-}
-Navigation.registerComponent('Home', () => HomeScreen);
-Navigation.registerComponent('Map', () => MapScreen);
+};
 
-// const Stack = createStackNavigator();
+const styles = StyleSheet.create({
+  safeView: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
+});
 
-//From NPM
-/*
-import {MFMapView, MFMarker, MFCircle, MFPolyline} from 'react-native-map4d-map'
-*/
-
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="Map">
-//         <Stack.Screen name="Home" component={HomeScreen} />
-//         <Stack.Screen name="Map" component={MapScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-// export default App;
-
-export default class App {
-  constructor() {
-    const stack = {
-      id:'AppStack',
-      children:[
-          {
-              component: {
-                  name: 'Map'
-              },
-              passProps: null,
-          }
-      ],
-      options: {
-          topBar: {
-              visible: false,
-              drawBehind: true,
-              animate: true,
-          }
-      }
-  }
-  // NavigationManager.addComponentId(stack.children[0].component.name)
-  Navigation.events().registerAppLaunchedListener(async () => {
-    Navigation.setRoot({
-      root: {
-        stack
-      }
-    });
-  });
-}
-}
-
+export default App;
